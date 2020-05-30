6282269edf3d5d3a4e61d125cc17952fe5aaada8
Abhishek Datta <dattaabhishek@rocketmail.com>
2020-05-30 12:45:20 +0530

    Fix table widths when job window changes from full-screen to maximized state

--- ui/src/app/components/job-window/job-window.component.ts
+++ ui/src/app/components/job-window/job-window.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NbWindowRef } from '@nebular/theme';
 import { AppService, Job } from 'src/app/app.service';
-import { filter } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'be-job-window',
@@ -14,49 +14,53 @@ export class JobWindowComponent implements OnInit, OnDestroy {
     loading = true;
     job: Job;
 
-    subscriptions: Subscription[];
+    private getsDestroyed = new Subject<void>();
 
     constructor(public windowRef: NbWindowRef,
                 private appService: AppService) {
     }
 
-        ngOnInit(): void {
-            this.fetchJob();
-            const jobCountSubscription = this.appService.getJobCounts()
-                                            .subscribe(_ => this.fetchJob());
-            const progressSubscription = this.appService.getProgress()
-                                            .pipe(filter(({ id }) => id === this.job?.id))
-                                            .subscribe(({ progress }) => this.job.progress = progress);
-            this.subscriptions = [jobCountSubscription, progressSubscription];
-        }
-
-        fetchJob() {
-            this.appService.getJob(this.windowRef.config.context as string)
-                .subscribe(job => {
-                    job.name = (!job.name || job.name === '__default__') ? job.data.name ?? job.name : job.name;
-                    this.job = job;
-                    this.loading = false;
-                });
-        }
-
-        getProgressBarStatus(progress: number) {
-            if (progress === 0) {
-                return 'basic';
-            }
-            if (progress < 10) {
-                return 'danger';
-            }
-            if (progress < 90) {
-                return 'warning';
-            }
-            if (progress < 100) {
-                return 'info';
-            }
-            return 'success';
-        }
-
-        ngOnDestroy() {
-            this.subscriptions.map(sub => sub.unsubscribe());
-        }
-
+    ngOnInit(): void {
+        this.fetchJob();
+        this.appService.getJobCounts()
+            .pipe(takeUntil(this.getsDestroyed))
+            .subscribe(_ => this.fetchJob());
+        this.appService.getProgress()
+            .pipe(
+                takeUntil(this.getsDestroyed),
+                filter(({ id }) => id === this.job?.id)
+            )
+            .subscribe(({ progress }) => this.job.progress = progress);
     }
+
+    fetchJob() {
+        this.appService.getJob(this.windowRef.config.context as string)
+            .subscribe(job => {
+                job.name = (!job.name || job.name === '__default__') ? job.data.name ?? job.name : job.name;
+                this.job = job;
+                this.loading = false;
+            });
+    }
+
+    getProgressBarStatus(progress: number) {
+        if (progress === 0) {
+            return 'basic';
+        }
+        if (progress < 10) {
+            return 'danger';
+        }
+        if (progress < 90) {
+            return 'warning';
+        }
+        if (progress < 100) {
+            return 'info';
+        }
+        return 'success';
+    }
+
+    ngOnDestroy() {
+        this.getsDestroyed.next();
+        this.getsDestroyed.complete();
+    }
+
+}
